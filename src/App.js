cc6e29215dce244e9dcc7ad266e6b13f232f75fe
Fernando <fernando.chaves16@hotmail.com>
2021-02-23 03:05:31 -0300

    stylized container and body

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
+import { useState } from 'react'
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
       </header>
     </div>
   );
