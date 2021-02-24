5c02b386b1d2210049679e2b55c5f258ec49c8c0
Fernando <fernando.chaves16@hotmail.com>
2021-02-24 03:52:40 -0300

    form and add btn stylized

--- src/components/ShowAddTask.js
+++ src/components/ShowAddTask.js
@@ -4,9 +4,9 @@ function ShowAddTask() {
     <form className="add-form">
       <div className="form-item">
         <label className="form-title">Enter your Todo below</label>
-        <input type="text" placeholder="Add Taskname"></input>
+        <input type="text" placeholder="Type your todo..." className="form-input"></input>
       </div>
-      <input type="submit" value="Submit"></input>
+      <input type="submit" value="Add Todo" className="form-btn"></input>
     </form>
   )
 }
